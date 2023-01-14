bab9abf2c94fff47bece7a75c138dfbdd9935f72
aiden6891 <102427259+aiden6891@users.noreply.github.com>
2023-01-14 11:29:05 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 var bgImage
-
+var gameState = "play";
+var score = 0;
 function preload() {
  bgImage = loadImage("images/Galaxy1.jpg");
  CharlieImage = loadImage("images/charlietheastronaut5.gif")
@@ -11,6 +12,7 @@ JupImg = loadImage("images/JUPITER.png")
 SatImg = loadImage("images/SATURN.png") 
 UraImg = loadImage("images/URANUS.png") 
 NepImg = loadImage("images/NEPTUNE.png") 
+AstImg = loadImage("Images/asteroid.png");
 }
 
 function setup() {
@@ -26,11 +28,12 @@ ground.visible = true;
 
 charlie = createSprite(100,450, 35, 35);
 charlie.addImage(CharlieImage);
-charlie.scale = 0.23
+charlie.scale = 0.18
 charlie.setCollider("circle", 0, 0, 460);
 charlie.debug = true; // with this you can be able to see the collider around the given image, if false then you can not see the image
 
-
+SatGroup = new Group();
+AstGroup = new Group();
 
  
 }
@@ -38,85 +41,112 @@ charlie.debug = true; // with this you can be able to see the collider around th
 function draw() {
   background(0);
   //console.log(bg.x);
+  if(gameState === "play"){
+
   if(bg.x < 0){
     bg.x = bg.width/4
   }
- charlie.collide(ground);
- if(keyDown("space")){
+charlie.collide(ground);
+if(keyDown("space")){
 charlie.velocityY = -10;
 
  }
  charlie.velocityY = charlie.velocityY + 0.8; // this gives the gravity
 
 SpawnObstacles();
+SpawnAsteroid();
+if(charlie.isTouching(SatGroup)){
+ score++;
 
-  drawSprites();
+}
+drawSprites();
+textSize(25);
+fill("white");
+text("Score: " +score , 200, 50);
+
+
+if(charlie.isTouching(AstGroup)){
+ gameState = "end"
+}
+  }
+  if(gameState === "end"){
+bg.velocityX = 0;
+charlie.velocityY = 0;
+AstGroup.setVelocityXEach(0);
+AstGroup.setLifetimeEach(-1);
+SatGroup.setVelocityXEach(0);
+SatGroup.setLifetimeEach(-1);
+
+  }
+  
 }
 
 function SpawnObstacles(){
 
-if(frameCount % 220 === 0){ // check after every 70 frame counts
-  var Mer = createSprite(600, 100, 10, 10);
-  var Ven = createSprite(600, 150, 10, 10);
-  var Ear = createSprite(600, 200, 10, 10);
-  var Mar = createSprite(600, 250, 10, 10);
-  var Jup = createSprite(600, 300, 10, 10);
-  var Sat = createSprite(600, 350, 10, 10);
-  var Ura = createSprite(600, 400, 10, 10);
-  var Nep = createSprite(600, 450, 10, 10);
-  Mer.velocityX = -5
-  Ven.velocityX = -5
-  Ear.velocityX = -5
-  Mar.velocityX = -5
-  Jup.velocityX = -5
-  Sat.velocityX = -5
-  Ura.velocityX = -5
-  Nep.velocityX = -5
+if(frameCount % 100 === 0){ // check after every 70 frame counts
+  var Sat = createSprite(1200, Math.round(random(50,400)), 10, 10);
+  
+
   var rand = Math.round(random(1,8));
   switch(rand){
-    case 1:Mer.addImage(MerImg);
-    Mer.scale = 0.5
+    case 1:Sat.addImage(MerImg);
+    Sat.velocityX = -5
+    Sat.scale = 0.3
           break 
-    case 2:Ven.addImage(VenImg);
-    Ven.scale = 0.5
+    case 2:Sat.addImage(VenImg);
+    Sat.velocityX = -5
+    Sat.scale = 0.3
           break
-    case 3:Ear.addImage(EarImg);
-    Ear.scale = 0.5
+    case 3:Sat.addImage(EarImg);
+    Sat.velocityX = -5
+    Sat.scale = 0.3
           break
-    case 4:Mar.addImage(MarImg);
-    Mar.scale = 0.5
+    case 4:Sat.addImage(MarImg);
+    Sat.velocityX = -5
+    Sat.scale = 0.3
            break
-    case 5:Jup.addImage(JupImg);
-    Jup.scale = 0.5
+    case 5:Sat.addImage(JupImg);
+    Sat.velocityX = -5
+    Sat.scale = 0.5
           break
     case 6:Sat.addImage(SatImg);
+    Sat.velocityX = -5
     Sat.scale = 0.5
            break
-    case 7:Ura.addImage(UraImg);
-    Ura.scale = 0.5
+    case 7:Sat.addImage(UraImg);
+    Sat.velocityX = -5
+    Sat.scale = 0.5
            break
-    case 8:Nep.addImage(NepImg);
-    Nep.scale = 0.5
+    case 8:Sat.addImage(NepImg);
+    Sat.velocityX = -5
+    Sat.scale = 0.3
            break
     default:break;
 
+  }
+  SatGroup.add(Sat);
+  Sat.lifetime = 250;
+}
 
-    
+}
+
+function SpawnAsteroid(){
+  if(frameCount % 100 === 0){ // check after every 70 frame counts
+    var Ast = createSprite(1200, Math.round(random(50,400)), 10, 10);
+    Ast.velocityX = -8
+    Ast.addImage(AstImg);
+    Ast.scale = 0.3
+     AstGroup.add(Ast);
+     Ast.lifetime = 250
 
 
 
   }
+ 
+  
 
-
-
-
-
-
+   
 }
-
-}
-
-
 
 
 
