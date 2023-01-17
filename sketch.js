d592a50cd7b9c35ae77ae764a358022228ebb5d6
aiden6891 <102427259+aiden6891@users.noreply.github.com>
2023-01-16 22:57:02 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -13,6 +13,9 @@ SatImg = loadImage("images/SATURN.png")
 UraImg = loadImage("images/URANUS.png") 
 NepImg = loadImage("images/NEPTUNE.png") 
 AstImg = loadImage("Images/asteroid.png");
+RestartImg = loadImage("Images/restartbutton.png");
+GameoverImg = loadImage("Images/gameover.png");
+sound = loadSound("Images/sound.mp3");
 }
 
 function setup() {
@@ -23,14 +26,20 @@ function setup() {
  bg.scale = 1.3;
  bg.velocityX = -3; 
 ground = createSprite(250, 600, 700, 10);
-ground.visible = true; 
-
-
+ground.visible = false;
+gameover = createSprite(600,200);
+restart = createSprite(600,350); 
+gameover.addImage(GameoverImg);
+restart.addImage(RestartImg);
+gameover.scale = 0.55;
+restart.scale = 0.3;
+gameover.visible = false;
+restart.visible = false;
 charlie = createSprite(100,450, 35, 35);
 charlie.addImage(CharlieImage);
 charlie.scale = 0.18
-charlie.setCollider("circle", 0, 0, 460);
-charlie.debug = true; // with this you can be able to see the collider around the given image, if false then you can not see the image
+charlie.setCollider("circle", 0, 0, 400);
+charlie.debug = false; // with this you can be able to see the collider around the given image, if false then you can not see the image
 
 SatGroup = new Group();
 AstGroup = new Group();
@@ -48,6 +57,7 @@ function draw() {
   }
 charlie.collide(ground);
 if(keyDown("space")){
+  //sound.play();
 charlie.velocityY = -10;
 
  }
@@ -57,33 +67,62 @@ SpawnObstacles();
 SpawnAsteroid();
 if(charlie.isTouching(SatGroup)){
  score++;
+SatGroup.destroyEach();
 
 }
-drawSprites();
-textSize(25);
-fill("white");
-text("Score: " +score , 200, 50);
+
 
 
 if(charlie.isTouching(AstGroup)){
  gameState = "end"
 }
   }
-  if(gameState === "end"){
+  else if (gameState === "end"){ 
 bg.velocityX = 0;
 charlie.velocityY = 0;
+gameover.visible = true;
+restart.visible = true;
 AstGroup.setVelocityXEach(0);
 AstGroup.setLifetimeEach(-1);
 SatGroup.setVelocityXEach(0);
 SatGroup.setLifetimeEach(-1);
 
   }
+  if(mousePressedOver(restart)){
+   AstGroup.destroyEach();
+    reset();
+    }
+  else if (gameState === "win"){
+    bg.velocityX = 0;
+    charlie.velocityY = 0;
+    //if(sound.isPlaying()){
+    //  sound.pause();
+   // }
   
+    AstGroup.setVelocityXEach(0);
+    AstGroup.setLifetimeEach(-1);
+    SatGroup.setVelocityXEach(0);
+    SatGroup.setLifetimeEach(-1);
+    
+      }
+drawSprites();
+textSize(25);
+fill("white");
+text("Score: " +score , 200, 50);
+if(score >= 5){
+  
+  fill("yellow");
+  textSize(40);
+  text("Congradulations!!! You have won the game!!!", 200,200);
+  gameState = "win";
+
+}
+ console.log(gameState);
 }
 
 function SpawnObstacles(){
 
-if(frameCount % 100 === 0){ // check after every 70 frame counts
+if(frameCount % 200 === 0){ // check after every 70 frame counts
   var Sat = createSprite(1200, Math.round(random(50,400)), 10, 10);
   
 
@@ -131,7 +170,7 @@ if(frameCount % 100 === 0){ // check after every 70 frame counts
 }
 
 function SpawnAsteroid(){
-  if(frameCount % 100 === 0){ // check after every 70 frame counts
+  if(frameCount % 50 === 0){ // check after every 70 frame counts
     var Ast = createSprite(1200, Math.round(random(50,400)), 10, 10);
     Ast.velocityX = -8
     Ast.addImage(AstImg);
@@ -140,6 +179,8 @@ function SpawnAsteroid(){
      Ast.lifetime = 250
 
 
+  Ast.setCollider("circle", 0, 0, 80);
+  Ast.debug = false; // with this you can be able to see the collider around the given image, if false then you can not see the image
 
   }
  
@@ -148,7 +189,14 @@ function SpawnAsteroid(){
    
 }
 
-
+function reset(){
+gameState = "play";
+gameover.visible = false;
+restart.visible = false;
+score = 0;
+//AstGroup.destory();
+SatGroup.destroyEach();
+}
 
 
 
